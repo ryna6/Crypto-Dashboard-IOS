ccbbde60b8ddd779ccedfa0f48f5ce1f7ed6a9ef
ryna6 <ryanjw629.h@gmail.com>
2025-11-22 04:07:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const TABS = [
     url: "https://www.coinglass.com/pro/futures/LiquidationMap",
     source: "CoinGlass",
     offsetTop: 420,   // Removes top section of the website
-    cropHeight: 900,  // Removes bottom section of the website embed
+    cropHeight: 880,  // Removes bottom section of the website embed
   },
   {
     id: "liqheat",
@@ -26,7 +26,7 @@ const TABS = [
     url: "https://www.coinglass.com/pro/futures/LiquidationHeatMap",
     source: "CoinGlass",
     offsetTop: 620,
-    cropHeight: 1140,
+    cropHeight: 1200,
   },
   {
     id: "spotflows",
